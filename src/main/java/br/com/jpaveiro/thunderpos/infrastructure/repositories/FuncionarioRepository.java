337fd90bd8ebef4ae22c9aaf7695cdfec02a2fbe
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-10-13 03:53:36 -0300

    feat: implement findByCpfAndEmail

--- src/main/java/br/com/jpaveiro/thunderpos/infrastructure/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/jpaveiro/thunderpos/infrastructure/repositories/FuncionarioRepository.java
@@ -4,7 +4,10 @@ import br.com.jpaveiro.thunderpos.infrastructure.entities.FuncionarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FuncionarioRepository extends JpaRepository<FuncionarioEntity, String> {
     Boolean existsByEmailOrCpf(String email, String cpf);
+    Optional<FuncionarioEntity> findByCpfAndEmail(String cpf, String email);
 }
