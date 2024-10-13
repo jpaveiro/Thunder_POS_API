287204ecd55a7acc71733b1bba77d13a063b9b50
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-10-13 02:51:37 -0300

    feat: dto for creating funcionario

---
+++ src/main/java/br/com/jpaveiro/thunderpos/infrastructure/dtos/CriarFuncionarioDTO.java
@@ -0,0 +1,16 @@
+package br.com.jpaveiro.thunderpos.infrastructure.dtos;
+
+import br.com.jpaveiro.thunderpos.domain.enums.ECargo;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record CriarFuncionarioDTO(
+        @NotNull(message = "Erro! Nome não preenchido.") @NotEmpty(message = "Erro! Nome não preenchido.") String nome,
+        @NotNull(message = "Erro! CPF não preenchido.") @NotEmpty(message = "Erro! CPF não preenchido.") String cpf,
+        @NotNull(message = "Erro! Senha não preenchida.") @NotEmpty(message = "Erro! Senha não preenchida.") String senha,
+        @NotNull(message = "Erro! E-mail não preenchido") @NotEmpty(message = "Erro! E-mail não preenchido.") String email,
+        @NotNull(message = "Erro! Cargo não preenchido.") ECargo cargo
+)
+{
+
+}
