a557f3344699ca84633415b31bc97179d550aa9b
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-10-13 02:53:16 -0300

    chore: enumeration 'cargo' converted to string and necessary args constructor

--- src/main/java/br/com/jpaveiro/thunderpos/infrastructure/entities/FuncionarioEntity.java
+++ src/main/java/br/com/jpaveiro/thunderpos/infrastructure/entities/FuncionarioEntity.java
@@ -1,12 +1,15 @@
 package br.com.jpaveiro.thunderpos.infrastructure.entities;
 
 import br.com.jpaveiro.thunderpos.domain.enums.ECargo;
+import br.com.jpaveiro.thunderpos.domain.enums.ENivelPermissao;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
@@ -16,6 +19,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Table(name = "tb_funcionarios")
 public class FuncionarioEntity extends UsuarioEntity {
+    @Enumerated(EnumType.STRING)
     private ECargo cargo;
 
     @OneToMany(mappedBy = "funcionario")
@@ -23,4 +27,9 @@ public class FuncionarioEntity extends UsuarioEntity {
 
     @OneToMany(mappedBy = "registradoPor")
     private Set<ProdutoEntity> produtosCriados;
+
+    public FuncionarioEntity(String id, String nome, String cpf, String senha, String email, ENivelPermissao nivelPermissao, LocalDateTime registradoEm, LocalDateTime alteradoEm, ECargo cargo) {
+        super(id, nome, cpf, senha, email, nivelPermissao, registradoEm, alteradoEm);
+        this.cargo = cargo;
+    }
 }
