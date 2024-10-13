07e969cea4ac5ab5d0f49a78aeeea3e11d1e98ac
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-10-13 17:43:55 -0300

    feat: create use case for listing Funcionarios

---
+++ src/main/java/br/com/jpaveiro/thunderpos/application/usecases/ListarFuncionariosUseCase.java
@@ -0,0 +1,36 @@
+package br.com.jpaveiro.thunderpos.application.usecases;
+
+import br.com.jpaveiro.thunderpos.application.mappers.FuncionarioMapper;
+import br.com.jpaveiro.thunderpos.domain.contracts.IUsecaseContract;
+import br.com.jpaveiro.thunderpos.domain.models.FuncionarioSecureDataModel;
+import br.com.jpaveiro.thunderpos.infrastructure.entities.FuncionarioEntity;
+import br.com.jpaveiro.thunderpos.infrastructure.repositories.FuncionarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ListarFuncionariosUseCase implements IUsecaseContract<Integer, List<FuncionarioSecureDataModel>> {
+    private final FuncionarioRepository repository;
+    private final FuncionarioMapper mapper;
+
+    public ListarFuncionariosUseCase(FuncionarioRepository repository, FuncionarioMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<FuncionarioSecureDataModel> run(Integer number) {
+        List<FuncionarioEntity> listaFuncionarios = repository.findAll();
+        List<FuncionarioSecureDataModel> secureList = new ArrayList<>();
+
+        for (FuncionarioEntity funcionario : listaFuncionarios) {
+            if (secureList.size() == number) {
+                break;
+            }
+            secureList.add(mapper.toSecureModel(funcionario));
+        }
+        return secureList;
+    }
+}
