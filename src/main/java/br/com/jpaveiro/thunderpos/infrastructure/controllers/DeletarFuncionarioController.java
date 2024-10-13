573938367c8bcbc1443a7f2e0d85f90c87777a96
jpaveiro <joaopedroaveiro2016@gmail.com>
2024-10-13 18:39:46 -0300

    feat: controller for deleting a Funcionario

---
+++ src/main/java/br/com/jpaveiro/thunderpos/infrastructure/controllers/DeletarFuncionarioController.java
@@ -0,0 +1,33 @@
+package br.com.jpaveiro.thunderpos.infrastructure.controllers;
+
+import br.com.jpaveiro.thunderpos.application.usecases.DeletarFuncionarioUsecase;
+import br.com.jpaveiro.thunderpos.domain.contracts.IControllerContract;
+import br.com.jpaveiro.thunderpos.domain.core.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/funcionario")
+public class DeletarFuncionarioController implements IControllerContract<String, StandardResponse> {
+    private final DeletarFuncionarioUsecase usecase;
+
+    public DeletarFuncionarioController(final DeletarFuncionarioUsecase usecase) {
+        this.usecase = usecase;
+    }
+
+
+    @Override
+    @DeleteMapping("/deletar")
+    public ResponseEntity<StandardResponse> perform(@RequestParam String id) {
+        usecase.run(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new StandardResponse(
+                        HttpStatus.OK,
+                        "Sucesso! A entidade foi deletada."
+                ));
+    }
+}
